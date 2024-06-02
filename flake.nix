6366dccdadd32af89426fd7006c4afc7b45eb60a
potatoesAreGod <>
2024-06-02 07:50:49 +0200

    moved home manager flake

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.user = import ./home.nix;
+          home-manager.users.user = import ./users/user/home.nix;
         }
       ];
     };
