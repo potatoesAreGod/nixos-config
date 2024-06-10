044b5b25cd7771708874867e48541f01a3c980df
potatoesAreGod <118043038+potatoesAreGod@users.noreply.github.com>
2024-06-10 16:55:19 +0200

    ssh now requires valid keys

--- users/user/default.nix
+++ users/user/default.nix
@@ -4,5 +4,6 @@
   users.users.user = {
     isNormalUser = true;
     extraGroups = [ "sudo" "wheel" ];
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEyYG14oAMvJetOad9IFVw8OLMs4ERZ7kbnuYdov3Jux ezra@computer.local" ];
   };
 }
